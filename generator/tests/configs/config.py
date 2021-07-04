b281598b9700c36c8c52efe5d7b335db5c5ffcbe
biarmic <biarmic@gmail.com>
2021-07-04 11:46:10 +0300

    Fix total_size of unit tests

--- generator/tests/configs/config.py
+++ generator/tests/configs/config.py
@@ -1,8 +1,8 @@
 # Common options for all tests
-total_size = 512
+total_size = 1024
 word_size = 8
 words_per_line = 4
 address_size = 11
 
 output_path = "outputs/unit_test/"
-verbose = 2
+verbose_level = 0
