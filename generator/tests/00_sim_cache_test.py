fdbd48394387e8f59f6acad914588f774147053e
biarmic <biarmic@gmail.com>
2021-07-08 18:44:21 +0300

    Combine sim_cache tests in one script

---
+++ generator/tests/00_sim_cache_test.py
@@ -0,0 +1,266 @@
+# See LICENSE for licensing information.
+#
+# Copyright (c) 2021 Regents of the University of California and The Board
+# of Regents for the Oklahoma Agricultural and Mechanical College
+# (acting for and on behalf of Oklahoma State University)
+# All rights reserved.
+#
+import unittest
+import sys
+import globals
+from globals import OPTS
+
+
+class sim_cache_test(unittest.TestCase):
+
+    def runTest(self):
+        # FIXME: Config file path may not be found
+        config_file = "tests/configs/config.py"
+
+        globals.init_opencache(config_file)
+
+        # Run tests for direct-mapped
+        OPTS.num_ways = 1
+        OPTS.replacement_policy = None
+        self.run_all_tests()
+
+        # Run tests for 2-way FIFO
+        OPTS.num_ways = 2
+        OPTS.replacement_policy = "fifo"
+        self.run_all_tests()
+
+        # Run tests for 2-way LRU
+        OPTS.num_ways = 2
+        OPTS.replacement_policy = "lru"
+        self.run_all_tests()
+
+        # Run tests for 2-way random
+        OPTS.num_ways = 2
+        OPTS.replacement_policy = "random"
+        self.run_all_tests()
+
+        globals.end_opencache()
+
+
+    def run_all_tests(self):
+
+        sc = setup_sim_cache()
+
+        self.assertTrue(check_reset(sc))
+
+        self.assertTrue(check_flush(sc))
+
+        self.assertTrue(check_hit(sc))
+
+        self.assertTrue(check_dirty(sc))
+
+        self.assertTrue(check_read_write(sc))
+
+        if OPTS.replacement_policy == "fifo":
+            self.assertTrue(check_fifo(sc))
+
+        if OPTS.replacement_policy == "lru":
+            self.assertTrue(check_lru(sc))
+
+        if OPTS.replacement_policy == "random":
+            self.assertTrue(check_random(sc))
+
+
+def setup_sim_cache():
+    """ Setup a sim_cache instance. """
+
+    from cache_config import cache_config
+    conf = cache_config(OPTS)
+
+    from sim_cache import sim_cache
+    sc = sim_cache(cache_config=conf)
+
+    return sc
+
+
+def reset(sc):
+    """ Reset a sim_cache instance. """
+
+    sc.reset()
+    sc.reset_dram()
+
+
+def check_reset(sc):
+    """ Check if reset() functions properly. """
+
+    # Write 1 to address 0
+    sc.write(0, 1)
+
+    # Reset the cache
+    sc.reset()
+
+    # Request to address 0 must be miss
+    if sc.is_hit(0):
+        return False
+
+    # Read unknown data from address 0
+    if sc.read(0) is not None:
+        return False
+
+    return True
+
+
+def check_flush(sc):
+    """ Check if flush() functions properly. """
+
+    reset(sc)
+
+    # Write 1 to address 0
+    sc.write(0, 1)
+
+    # Flush and reset the cache
+    sc.flush()
+    sc.reset()
+
+    # Read 1 from address 0
+    if sc.read(0) != 1:
+        return False
+
+    return True
+
+
+def check_hit(sc):
+    """ Check if is_hit() functions properly. """
+
+    reset(sc)
+
+    # Address 0 is miss
+    if sc.is_hit(0):
+        return False
+
+    # Read from address 0
+    sc.read(0)
+
+    # Address 0 is hit
+    if not sc.is_hit(0):
+        return False
+
+    return True
+
+
+def check_dirty(sc):
+    """ Check if is_dirty() functions properly. """
+
+    reset(sc)
+
+    # Read from address 0
+    sc.read(0)
+
+    # Address 0 is not dirty
+    if sc.is_dirty(0):
+        return False
+
+    # Write 1 to address 0
+    sc.write(0, 1)
+
+    # Address 0 is dirty
+    if not sc.is_dirty(0):
+        return False
+
+    return True
+
+
+def check_read_write(sc):
+    """ Check if read() and write() function properly. """
+
+    reset(sc)
+
+    # Read unknown from address 0
+    if sc.read(0) is not None:
+        return False
+
+    # Write 1 from address 0
+    sc.write(0, 1)
+
+    # Read 1 from address 0
+    if sc.read(0) != 1:
+        return False
+
+    return True
+
+
+def check_fifo(sc):
+    """ Check FIFO replacement of sim_cache. """
+
+    reset(sc)
+
+    # Write 1 to address_0
+    address_0 = sc.merge_address(0, 0, 0)
+    sc.write(address_0, 1)
+
+    # Write 2 to address_1
+    address_1 = sc.merge_address(1, 0, 0)
+    sc.write(address_1, 2)
+
+    # Write 3 to address_2
+    address_2 = sc.merge_address(2, 0, 0)
+    sc.write(address_2, 3)
+
+    # address_0 must be replaced
+    if sc.is_hit(address_0):
+        return False
+
+    # Read 1 from address_0
+    if sc.read(address_0) != 1:
+        return False
+
+    # address_1 must be replaced
+    if sc.is_hit(address_1):
+        return False
+
+    return True
+
+
+def check_lru(sc):
+    """ Check LRU replacement of sim_cache. """
+
+    reset(sc)
+
+    # Write 1 to address_0
+    address_0 = sc.merge_address(0, 0, 0)
+    sc.write(address_0, 1)
+
+    # Write 2 to address_1
+    address_1 = sc.merge_address(1, 0, 0)
+    sc.write(address_1, 2)
+
+    sc.read(address_0)
+
+    # Write 3 to address_2
+    address_2 = sc.merge_address(2, 0, 0)
+    sc.write(address_2, 3)
+
+    # address_1 must be replaced
+    if sc.is_hit(address_1):
+        return False
+
+    # Read 2 from address_1
+    if sc.read(address_1) != 2:
+        return False
+
+    # address_0 must be replaced
+    if sc.is_hit(address_0):
+        return False
+
+    return True
+
+
+def check_random(sc):
+    """ Check random replacement of sim_cache. """
+
+    reset(sc)
+
+    # TODO: sim_cache must imitate the random counter.
+    return False
+
+
+# Run the test from the terminal
+if __name__ == "__main__":
+    (OPTS, args) = globals.parse_args()
+    del sys.argv[1:]
+    unittest.main()
