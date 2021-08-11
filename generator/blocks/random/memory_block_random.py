640837fa149bdb8db3237f225a55cd2f99eaad0b
biarmic <eren.dogan@ozu.edu.tr>
2021-08-11 12:07:29 +0300

    Fix file names for random

--- generator/blocks/random/memory_block_random.py
+++ generator/blocks/random/memory_block_random.py
@@ -5,13 +5,14 @@
 # (acting for and on behalf of Oklahoma State University)
 # All rights reserved.
 #
-from memory_block_base import memory_block_base
+from state_block_base import state_block_base
+from nmigen import Cat
 from state import State
 
 
-class memory_block_random(memory_block_base):
+class state_block_random(state_block_base):
     """
-    This class extends base memory controller module for random replacement
+    This class extends base state controller module for random replacement
     policy.
     """
 
@@ -23,42 +24,67 @@ class memory_block_random(memory_block_base):
     def add_compare(self, dsgn, m):
         """ Add statements for the COMPARE state. """
 
-        # In the COMPARE state, state switches to:
-        #   IDLE        if current request is hit and CPU isn't sending a new request
-        #   COMPARE     if current request is hit and CPU is sending a new request
-        #   WAIT_HAZARD if current request is hit and data hazard is possible
-        #   WRITE       if current request is dirty miss and DRAM is busy
-        #   WAIT_WRITE  if current request is dirty miss and DRAM is available
-        #   READ        if current request is clean miss and DRAM is busy
-        #   WAIT_READ   if current request is clean miss and DRAM is available
+        # In the COMPARE state, cache compares tags.
+        # Stall and dout are driven by the Output Block.
         with m.Case(State.COMPARE):
             # Assuming that current request is miss, check if it is dirty miss
             with dsgn.check_dirty_miss(m, dsgn.random):
+                # If DRAM is busy, switch to WRITE and wait for DRAM to be available
                 with m.If(dsgn.main_stall):
-                    m.d.comb += dsgn.state.eq(State.WRITE)
+                    m.d.comb += dsgn.tag_read_addr.eq(dsgn.set)
+                    m.d.comb += dsgn.data_read_addr.eq(dsgn.set)
+                # If DRAM is available, switch to WAIT_WRITE and wait for DRAM to
+                # complete writing
                 with m.Else():
-                    m.d.comb += dsgn.state.eq(State.WAIT_WRITE)
+                    m.d.comb += dsgn.main_csb.eq(0)
+                    m.d.comb += dsgn.main_web.eq(0)
+                    m.d.comb += dsgn.main_addr.eq(Cat(dsgn.set, dsgn.tag_read_dout.tag(dsgn.random)))
+                    m.d.comb += dsgn.main_din.eq(dsgn.data_read_dout.line(dsgn.random))
             # Else, assume that current request is clean miss
             with dsgn.check_clean_miss(m):
-                with m.If(dsgn.main_stall):
-                    m.d.comb += dsgn.state.eq(State.READ)
-                with m.Else():
-                    m.d.comb += dsgn.state.eq(State.WAIT_READ)
+                with m.If(~dsgn.main_stall):
+                    m.d.comb += dsgn.tag_read_addr.eq(dsgn.set)
+                    m.d.comb += dsgn.data_read_addr.eq(dsgn.set)
+                    m.d.comb += dsgn.main_csb.eq(0)
+                    m.d.comb += dsgn.main_addr.eq(Cat(dsgn.set, dsgn.tag))
+            # Check if there is an empty way. All empty ways need to be filled
+            # before evicting a random way.
             for i in range(dsgn.num_ways):
                 with m.If(~dsgn.tag_read_dout.valid(i)):
-                    with m.If(dsgn.main_stall):
-                        m.d.comb += dsgn.state.eq(State.READ)
-                    with m.Else():
-                        m.d.comb += dsgn.state.eq(State.WAIT_READ)
-            # Check if current request is hit.
+                    with m.If(~dsgn.main_stall):
+                        m.d.comb += dsgn.tag_read_addr.eq(dsgn.set)
+                        m.d.comb += dsgn.data_read_addr.eq(dsgn.set)
+                        m.d.comb += dsgn.main_csb.eq(0)
+                        m.d.comb += dsgn.main_web.eq(1)
+                        m.d.comb += dsgn.main_addr.eq(Cat(dsgn.set, dsgn.tag))
+            # Check if current request is hit
             # Compare all ways' tags to find a hit. Since each way has a different
             # tag, only one of them can match at most.
             for i in range(dsgn.num_ways):
                 with dsgn.check_hit(m, i):
-                    with m.If(dsgn.csb):
-                        m.d.comb += dsgn.state.eq(State.IDLE)
-                    with m.Else():
-                        with m.If(~dsgn.web_reg & (dsgn.set == dsgn.addr.parse_set())):
-                            m.d.comb += dsgn.state.eq(State.WAIT_HAZARD)
-                        with m.Else():
-                            m.d.comb += dsgn.state.eq(State.COMPARE)
+                    # Set DRAM's csb to 1 again since it could be set 0 above
+                    m.d.comb += dsgn.main_csb.eq(1)
+                    # Perform the write request
+                    with m.If(~dsgn.web_reg):
+                        # Update dirty bit in the tag line
+                        m.d.comb += dsgn.tag_write_csb.eq(0)
+                        m.d.comb += dsgn.tag_write_addr.eq(dsgn.set)
+                        m.d.comb += dsgn.tag_write_din.eq(dsgn.tag_read_dout)
+                        m.d.comb += dsgn.tag_write_din.dirty(i).eq(1)
+                        # Write the word over the write mask
+                        # NOTE: This switch statement is written manually (not only with
+                        # word_select) because word_select fails to generate correct case
+                        # statements if offset calculation is a bit complex.
+                        m.d.comb += dsgn.data_write_csb.eq(0)
+                        m.d.comb += dsgn.data_write_addr.eq(dsgn.set)
+                        m.d.comb += dsgn.data_write_din.eq(dsgn.data_read_dout)
+                        for j in range(dsgn.num_bytes):
+                            with m.If(dsgn.wmask_reg[j]):
+                                with m.Switch(dsgn.offset):
+                                    for k in range(dsgn.words_per_line):
+                                        with m.Case(k):
+                                            m.d.comb += dsgn.data_write_din.byte(j, k, i).eq(dsgn.din_reg.byte(j))
+                    # Read next lines from SRAMs even though CPU is not
+                    # sending a new request since read is non-destructive.
+                    m.d.comb += dsgn.tag_read_addr.eq(dsgn.addr.parse_set())
+                    m.d.comb += dsgn.data_read_addr.eq(dsgn.addr.parse_set())
