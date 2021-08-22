b251b95e5d6d7c68fc091fdd26501cca0d02c2d5
biarmic <eren.dogan@ozu.edu.tr>
2021-08-22 14:25:03 +0300

    Add unit tests for write mask

---
+++ generator/tests/04_wmask_nway_fifo_test.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+# See LICENSE for licensing information.
+#
+# Copyright (c) 2021 Regents of the University of California and The Board
+# of Regents for the Oklahoma Agricultural and Mechanical College
+# (acting for and on behalf of Oklahoma State University)
+# All rights reserved.
+#
+import unittest
+import sys
+import globals
+from testutils import *
+from base.policy import ReplacementPolicy as RP
+from globals import OPTS
+
+
+class verify_test(opencache_test):
+
+    def runTest(self):
+
+        OPENCACHE_HOME = os.getenv("OPENCACHE_HOME")
+        config_file = "{}/tests/configs/config.py".format(OPENCACHE_HOME)
+        globals.init_opencache(config_file)
+
+        OPTS.write_size = 8
+        OPTS.num_ways = 4
+        OPTS.replacement_policy = RP.FIFO
+        OPTS.simulate = True
+        OPTS.synthesize = True
+
+        conf = make_config()
+
+        from cache import cache
+        c = cache(cache_config=conf,
+                  name=OPTS.output_name)
+        c.save()
+
+        from verify import verify
+        v = verify(cache_config=conf,
+                   name=OPTS.output_name)
+        self.check_verification(v)
+
+        globals.end_opencache()
+
+
+# Run the test from the terminal
+if __name__ == "__main__":
+    (OPTS, args) = globals.parse_args()
+    del sys.argv[1:]
+    header(__file__)
+    unittest.main()
