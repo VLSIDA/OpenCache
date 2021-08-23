255e20dfb75cf9a31e103cf4cd85319e14736794
biarmic <eren.dogan@ozu.edu.tr>
2021-08-23 12:17:22 +0300

    Add setup scripts for path

---
+++ setpaths.csh
@@ -0,0 +1,7 @@
+#!/bin/csh
+# This is a csh utility script to set the paths to the current
+# directory of OpenCache. It must be sourced in the local directory
+# like this:
+# source setpaths.csh
+
+setenv OPENCACHE_HOME="`pwd`/generator"
---
+++ setpaths.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# This is a bash utility script to set the paths to the current
+# directory of OpenCache. It must be sourced in the local directory
+# like this:
+# source setpaths.sh
+
+export OPENCACHE_HOME="`pwd`/generator"
