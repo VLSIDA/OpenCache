2f6d7d2fd8a26b07e9a8172935c9e5769aac8cd1
erendo <eren.dogan@ozu.edu.tr>
2022-01-17 18:51:33 +0300

    Merge memory blocks

--- generator/blocks/write_back/base/memory_block_base.py
+++ generator/blocks/write_back/base/memory_block_base.py
@@ -91,9 +91,9 @@ class memory_block_base(block_base):
                 # Assuming that current request is miss, check if it is dirty miss
                 if is_dirty:
                     # If DRAM is available, switch to WAIT_WRITE and wait for DRAM to
-                    # complete writing
+                    # complete writing.
                     with m.If(~c.dram.stall()):
-                        c.dram.write(Cat(c.set, c.tag_array.output().tag()), c.data_array.output())
+                        c.dram.write(Cat(c.set, c.tag_array.output().tag(i)), c.data_array.output(i))
                 # Else, assume that current request is clean miss
                 else:
                     # If DRAM is busy, switch to READ and wait for DRAM to be available
@@ -101,17 +101,31 @@ class memory_block_base(block_base):
                     # complete reading
                     with m.If(~c.dram.stall()):
                         c.dram.read(Cat(c.set, c.tag))
+            # Check if there is an empty way. All empty ways need to be filled
+            # before evicting a random way.
+            # NOTE: The line below should only work for some replacement policies where
+            # the lines above may miss an empty way (such as random replacement).
+            for i in c.hit_detector.find_empty():
+                # If DRAM is busy, switch to READ and wait for DRAM to be available
+                # If DRAM is available, switch to WAIT_READ and wait for DRAM to
+                # complete reading
+                with m.If(~c.dram.stall()):
+                    c.dram.read(Cat(c.set, c.tag))
             # Check if current request is hit
+            # Compare all ways' tags to find a hit. Since each way has a different
+            # tag, only one of them can match at most.
+            # NOTE: This for loop should not be merged with the one above since hit
+            # should be checked after all miss assumptions are done.
             for i in c.hit_detector.find_hit():
-                # Set DRAM's csb to 1 again since it could be set 0 above
+                # Disable DRAM since a request could have been sent above
                 c.dram.disable()
                 # Perform the write request
                 with m.If(~c.web_reg):
                     # Update dirty bit
-                    c.tag_array.write(c.set, Cat(c.tag, C(3, 2)))
+                    c.tag_array.write(c.set, Cat(c.tag, C(3, 2)), i)
                     # Perform write request
-                    c.data_array.write(c.set, c.data_array.output())
-                    c.data_array.write_input(0, c.offset, c.din_reg, c.wmask_reg if c.num_masks else None)
+                    c.data_array.write(c.set, c.data_array.output(i), i)
+                    c.data_array.write_input(i, c.offset, c.din_reg, c.wmask_reg if c.num_masks else None)
                 # Read next lines from SRAMs even though the CPU is not sending
                 # a new request since read is non-destructive.
                 c.tag_array.read(c.addr.parse_set())
